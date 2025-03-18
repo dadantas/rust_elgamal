d371f7cbf5e5d2b89523f869f2a175b484767ebb
David Dantas <davidalexandredantas@gmail.com>
2025-03-18 11:31:14 +0000

    feat: Remove OpenSSL dependency and updated AES benchmarks to use aes crate

--- benches/aes.rs
+++ benches/aes.rs
@@ -1,31 +1,45 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use openssl::symm::Cipher;
+use aes::Aes128;
+use aes::cipher::{
+    BlockCipher, BlockEncrypt, BlockDecrypt, KeyInit,
+    generic_array::GenericArray,
+};
 
 
 //benchmarks for aes ctr
 
 fn bench_encrypt(c: &mut Criterion) {
-    let plain = "Hello, world!";
-    let key = "0123456789abcdef";
+    let key = GenericArray::from([0u8; 16]);
+    let mut block = GenericArray::from([42u8; 16]);
+    
+    // Initialize cipher
+    let cipher = Aes128::new(&key);
+    
+    let block_copy = block.clone();
     let iv = "0123456789abcdef";
-    let cipher = Cipher::aes_128_ctr();
+    let cipher = Aes128::new(&key);
     c.bench_function("AES/CTR Encryption", |b| {
         b.iter(|| {
-            openssl::symm::encrypt(cipher, key.as_bytes(), Some(iv.as_bytes()), plain.as_bytes()).unwrap()
+            cipher.encrypt_block(&mut block);
         })
     });
 }
 
 
 fn bench_decrypt(c: &mut Criterion) {
-    let plain = "Hello, world!";
-    let key = "0123456789abcdef";
+    let key = GenericArray::from([0u8; 16]);
+    let mut block = GenericArray::from([42u8; 16]);
+    
+    // Initialize cipher
+    let cipher = Aes128::new(&key);
+    
+    let block_copy = block.clone();
     let iv = "0123456789abcdef";
-    let cipher = Cipher::aes_128_ctr();
-    let encrypted = openssl::symm::encrypt(cipher, key.as_bytes(), Some(iv.as_bytes()), plain.as_bytes()).unwrap();
+    let cipher = Aes128::new(&key);
+    let encrypted = cipher.encrypt_block(&mut block);
     c.bench_function("AES/CTR Decryption", |b| {
         b.iter(|| {
-            openssl::symm::decrypt(cipher, key.as_bytes(), Some(iv.as_bytes()), &encrypted).unwrap()
+            cipher.decrypt_block(&mut block);
         })
     });
 }
--- src/main.rs
+++ src/main.rs
@@ -2,31 +2,16 @@
 use std::time::Instant;
 
 
-use openssl::symm::Cipher;
 use hex::encode;
 use rust_elgamal::{decrypt_message, encrypt_message, free_buffer, free_keypair, generate_keypair, rerandomize_ciphertext};
 
 fn main() {
-    //aes ctr
-    let plain = "Hello, world!";
-    let key = "0123456789abcdef";
-    let iv = "0123456789abcdef";
-    let cipher = Cipher::aes_128_ctr();
-    let mut start = Instant::now();
-    let encrypted = openssl::symm::encrypt(cipher, key.as_bytes(), Some(iv.as_bytes()), plain.as_bytes()).unwrap();
-    println!("AES/CTR Enc Time: {:?}", start.elapsed());
-    start = Instant::now();
-    let decrypted = openssl::symm::decrypt(cipher, key.as_bytes(), Some(iv.as_bytes()), &encrypted).unwrap();
-    println!("AES/CTR Dec Time: {:?}", start.elapsed());
-    println!("Encrypted: {}", encode(&encrypted));
-    println!("Decrypted: {}", String::from_utf8(decrypted).unwrap());
-
 
     let keypair = generate_keypair();
     let keypair_ref = unsafe { &*keypair };
 
     let message = "Hello, world!";
-    start = Instant::now();
+    let start = Instant::now();
     let mut ciphertext = encrypt_message(keypair_ref.pub_key.as_ptr(), message.as_ptr(), message.len());
     println!("ElGamal Enc Time: {:?}", start.elapsed());
 
